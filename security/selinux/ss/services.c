c2c9726a42ce95c09313d423a4a0e1fa00d01523
Kyeongdon Kim <kyeongdon.kim@lge.com>
2017-09-06 18:50:19 +0900

    UPSTREAM: selinux: Use kmem_cache for hashtab_node
    During random test as own device to check slub account,
    we found some slack memory from hashtab_node(kmalloc-64).
    By using kzalloc(), middle of test result like below:
    allocated size 240768
    request size 45144
    slack size 195624
    allocation count 3762
    
    So, we want to use kmem_cache_zalloc() and that
    can reduce memory size 52byte(slack size/alloc count) per each struct.
    
    Signed-off-by: Kyeongdon Kim <kyeongdon.kim@lge.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    (cherry picked from commit 7c620ece125cbab7b5dfcb574ee1e64ab8b562cd)
    Change-Id: I23f426ba037842ffb03287846e484a281230902b
    Bug: 140252993
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Twisted <36546624+TwistedPrime@users.noreply.github.com>
    Signed-off-by: madeofgreat <ravenklawasd@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: Dhruv <dhruvgera61@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2031,10 +2031,12 @@ int security_load_policy(void *data, size_t len)
 	if (!ss_initialized) {
 		avtab_cache_init();
 		ebitmap_cache_init();
+		hashtab_cache_init();
 		rc = policydb_read(&policydb, fp);
 		if (rc) {
 			avtab_cache_destroy();
 			ebitmap_cache_destroy();
+			hashtab_cache_destroy();
 			goto out;
 		}
 
@@ -2046,6 +2048,7 @@ int security_load_policy(void *data, size_t len)
 			policydb_destroy(&policydb);
 			avtab_cache_destroy();
 			ebitmap_cache_destroy();
+			hashtab_cache_destroy();
 			goto out;
 		}
 
@@ -2054,6 +2057,7 @@ int security_load_policy(void *data, size_t len)
 			policydb_destroy(&policydb);
 			avtab_cache_destroy();
 			ebitmap_cache_destroy();
+			hashtab_cache_destroy();
 			goto out;
 		}
 
