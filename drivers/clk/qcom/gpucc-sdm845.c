56afdd402a6b9f606e91cfc3822032236cae4445
Dhruv <Dhruvgera61@gmail.com>
2021-02-24 16:23:19 +0530

    beryllium: Ramp up the GPU frequency
    Signed-off-by: Dhruv <Dhruvgera61@gmail.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -282,7 +282,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-        F(800000000, P_CRC_DIV,  1, 0, 0),
+        F(850000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -640,7 +640,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		596000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		800000000;
+		850000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
