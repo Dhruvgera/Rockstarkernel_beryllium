cbbd068efb0d44246b154ab0a36e3994d2ca57a3
Dhruv <dhruvgera61@gmail.com>
2022-07-31 11:41:34 +0200

    Revert "drivers: drm: msm: Drop custom doze impl and backlight hax from Xiaomi"
    This reverts commit 64f74905fe058e9a0d51d9b9fe9038d0ffd0636f.

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.h
@@ -589,6 +589,8 @@ void dsi_display_enable_event(struct dsi_display *display,
 
 int dsi_display_set_backlight(void *display, u32 bl_lvl);
 
+int dsi_panel_set_doze_backlight(struct dsi_display *display, u32 bl_lvl);
+
 /**
  * dsi_display_check_status() - check if panel is dead or alive
  * @display:            Handle to display.
