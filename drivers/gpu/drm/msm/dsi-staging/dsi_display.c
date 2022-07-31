de13ec41b4035bc1f98c20f5a7399c1c88952f56
Dhruv <dhruvgera61@gmail.com>
2022-07-31 11:41:14 +0200

    Revert "drm: msm: dsi-staging: add get_main_display"
    This reverts commit 4682a400bd2011f035aa85df675d5adaeaaad45d.

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5921,7 +5921,6 @@ int dsi_display_get_modes(struct dsi_display *display,
 exit:
 	*out_modes = display->modes;
 	rc = 0;
-	primary_display = display;
 
 error:
 	if (rc)
@@ -7139,10 +7138,6 @@ int dsi_display_unprepare(struct dsi_display *display)
 	mutex_unlock(&display->display_lock);
 	SDE_EVT32(SDE_EVTLOG_FUNC_EXIT);
 	return rc;
-}
-
-struct dsi_display *get_main_display(void) {
-	return primary_display;
 }
 
 static int __init dsi_display_register(void)
